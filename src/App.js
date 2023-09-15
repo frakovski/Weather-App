7e8baae1c871280443fa8b32c0c80846e435bf58
frakovski <fermanliferman11@gmail.com>
2023-09-15 19:01:42 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [weatherData, setWeatherData] = useState(null)
+  const [location, setLocation] = useState('')
+
+  const apiKey = "ebd96dd82c3a44a48f7132754231509"
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+
+        const response = await axios.get(
+          `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=4&aqi=yes&alerts=yes`)
+        setWeatherData(response.data)
+        console.log(response);
+
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    if (location) {
+      fetchData()
+    }
+  }, [location])
+
+  const handleLocationChange = (event) => {
+    setLocation(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-container'>
+      <h1 className="app-title">Weather app</h1>
+      <div className='input-container'>
+        <input
+          className='location-input'
+          type='text'
+          placeholder='Enter City'
+          value={location}
+          onChange={handleLocationChange}
+        />
+      </div>
+
+      {weatherData && (
+        <div className='weather-container'>
+
+          {
+            weatherData.forecast.forecastday.map((day) => (
+              <div className='day-container' key={day.date}>
+                <h2 className='date'>{day.date}</h2>
+                <img className='weather-icon' src={day.day.condition.icon} alt={day.day.condition.text}/>
+                <p className='temperature'>{day.day.avgtemp_c}C</p>
+                <p className='temperature'>{day.day.condition.text}</p>
+              </div>
+            ))
+          }
+        </div>
+      )}
+
     </div>
   );
 }
